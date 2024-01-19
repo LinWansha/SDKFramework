93e59f9bcf90b9988404d1f017e7cd842abbe72a
mengruiqing <mengruiqing@habby.com>
2024-01-19 16:16:27 +0800

    1.webView链接配置表通过EditorWindow自动化  2.EditorWindow规范命名和美化

---
+++ HABBY_CN/Assets/SDKFramework/Editor/Configuration_AgeTip_Window.cs
@@ -0,0 +1,83 @@
+namespace SDKFramework.Editor
+{
+    using System.IO;
+    using SDKFramework.Config;
+    using UnityEditor;
+    using UnityEngine;
+
+    public class Configuration_AgeTip_Window : EditorWindow
+    {
+        private string configFilePath = "Assets/StreamingAssets/SDKConfig/AgeTipConfig.json";
+        private AgeTipConfig _ageTipConfig;
+        
+        private Vector2 _scrollPosition;
+        private GUIStyle headerStyle;
+        private const string Tittle = "AgeTip Configuration Linker";
+        
+        [MenuItem("SDKFramework/Configuration Age Tip")]
+        private static void ShowWindow()
+        {
+            var window =GetWindow<Configuration_AgeTip_Window>(Tittle);
+            window.maxSize = new Vector2(1800, 800);
+            window.minSize = window.maxSize;
+        }
+
+        private void OnEnable()
+        {
+            headerStyle = new GUIStyle
+            {
+                fontSize = 14,
+                fontStyle = FontStyle.Bold,
+                alignment = TextAnchor.MiddleCenter,
+                normal = new GUIStyleState() { textColor = Color.green }
+            };
+            _ageTipConfig = LoadConfiguration();
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField(Tittle, headerStyle);
+            EditorGUILayout.Space();
+
+            EditorGUILayout.LabelField("Configuration file path: " + configFilePath);
+            EditorGUILayout.Space();
+
+            _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
+
+            _ageTipConfig.gameName = EditorGUILayout.TextField("Game Name", _ageTipConfig.gameName);
+            _ageTipConfig.applicableRange =
+                (AgeTipConfig.ApplicableRange)EditorGUILayout.EnumPopup("Applicable Range",
+                    _ageTipConfig.applicableRange);
+            _ageTipConfig.details =
+                EditorGUILayout.TextArea(_ageTipConfig.details, GUILayout.Height(position.height / 2));
+
+            if (GUILayout.Button("Save Configuration"))
+            {
+                SaveConfiguration();
+            }
+
+            EditorGUILayout.EndScrollView();
+        }
+
+        private AgeTipConfig LoadConfiguration()
+        {
+            if (!File.Exists(configFilePath))
+            {
+                Debug.LogWarning("Configuration file not found! Please ensure the file exists at: " + configFilePath);
+                return new AgeTipConfig(); // 返回默认对象
+            }
+
+            string json = File.ReadAllText(configFilePath);
+            return JsonUtility.FromJson<AgeTipConfig>(json);
+        }
+
+        private void SaveConfiguration()
+        {
+            string json = JsonUtility.ToJson(_ageTipConfig, true);
+            File.WriteAllText(configFilePath, json);
+            AssetDatabase.Refresh();
+            Debug.Log("Configuration saved to: " + configFilePath);
+        }
+    }
+}
---
+++ HABBY_CN/Assets/SDKFramework/Editor/Configuration_WebURL_Window.cs
@@ -0,0 +1,106 @@
+namespace SDKFramework.Editor
+{
+    using UnityEditor;
+    using UnityEngine;
+    using System.IO;
+
+    public class Configuration_WebURL_Window : EditorWindow
+    {
+        private string gameLicenseUrl = "";
+        private string privacyUrl = "";
+        private string childrenProtUrl = "";
+        private string thirdPartySharingUrl = "";
+
+        // 配置文件路径
+        private string streamingAssetsDir = "Assets/StreamingAssets/SDKConfig";
+        private string configFileName = "WebConfig.json";
+
+        private GUIStyle headerStyle;
+        private const string Tittle = "WebUrl Configuration Linker";
+        [MenuItem("SDKFramework/Configuration WebURL")]
+        public static void ShowWindow()
+        {
+            var window =   GetWindow<Configuration_WebURL_Window>(Tittle);
+            window.maxSize = new Vector2(600, 250);
+            window.minSize = window.maxSize;
+        }
+
+        private void OnEnable()
+        {
+            headerStyle = new GUIStyle
+            {
+                fontSize = 14,
+                fontStyle = FontStyle.Bold,
+                alignment = TextAnchor.MiddleCenter,
+                normal = new GUIStyleState() { textColor = Color.green }
+            };
+            // 从配置文件中加载已有链接
+            LoadExistingLinks();
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.Space();
+            GUILayout.Label(Tittle, headerStyle);
+            EditorGUILayout.Space();
+            
+            gameLicenseUrl = EditorGUILayout.TextField("Game License URL:", gameLicenseUrl);
+            privacyUrl = EditorGUILayout.TextField("Privacy URL:", privacyUrl);
+            childrenProtUrl = EditorGUILayout.TextField("Children Prot URL:", childrenProtUrl);
+            thirdPartySharingUrl = EditorGUILayout.TextField("Third Party Sharing URL:", thirdPartySharingUrl);
+
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Save Links"))
+            {
+                SaveLinks();
+            }
+        }
+
+        private void LoadExistingLinks()
+        {
+            string configFilePath = Path.Combine(streamingAssetsDir, configFileName);
+
+            if (File.Exists(configFilePath))
+            {
+                string jsonContent = File.ReadAllText(configFilePath);
+                var configData = JsonUtility.FromJson<ConfigData>(jsonContent);
+
+                gameLicenseUrl = configData.gameLicenseUrl;
+                privacyUrl = configData.privacyUrl;
+                childrenProtUrl = configData.childrenProtUrl;
+                thirdPartySharingUrl = configData.thirdPartySharingUrl;
+            }
+            else
+            {
+                Debug.LogError(
+                    "Failed to load existing links. Make sure the WebConfig.json file exists and has the correct format.");
+            }
+        }
+
+        private void SaveLinks()
+        {
+            var configData = new ConfigData
+            {
+                gameLicenseUrl = gameLicenseUrl,
+                privacyUrl = privacyUrl,
+                childrenProtUrl = childrenProtUrl,
+                thirdPartySharingUrl = thirdPartySharingUrl
+            };
+
+            string jsonContent = JsonUtility.ToJson(configData, true);
+            File.WriteAllText(Path.Combine(streamingAssetsDir, configFileName), jsonContent);
+
+            Debug.Log("Links saved to WebConfig.json");
+        }
+    }
+
+    [System.Serializable]
+    public class ConfigData
+    {
+        public string gameLicenseUrl;
+        public string privacyUrl;
+        public string childrenProtUrl;
+        public string thirdPartySharingUrl;
+    }
+}
